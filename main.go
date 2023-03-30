3a2ef7726c25d69f5be03d855b941ddbc0ca8f05
NPG27 <nicolas.potes@mercadolibre.com.co>
2023-03-30 04:01:54 -0500

    main

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/bootcamp-go/desafio-go-bases/internal/tickets"
 )
 
 func main() {
-	total, err := tickets.GetTotalTickets("Brazil")
+	// Cargar tickets
+	ticketsSlice, errLoadTickets := tickets.LoadTickets("tickets.csv")
+	if errLoadTickets != nil {
+		fmt.Println("Error cargando los tickets", errLoadTickets)
+		return
+	}
+
+	// Llamado a Requerimiento 1
+	total, _ := tickets.GetTotalTickets(ticketsSlice, "Finland")
+	fmt.Println("Requerimiento 1:", total)
+
+	// Llamado a Requerimiento 2
+	mapByPeriod := tickets.GetCountByPeriod(ticketsSlice)
+	fmt.Println("Requerimiento 2:", mapByPeriod)
+
+	//Llamado a Requerimiento 3
+	averageByDestination, _ := tickets.AverageDestination(ticketsSlice, "Finland")
+	fmt.Println("Requerimiento 3:", averageByDestination)
 }
