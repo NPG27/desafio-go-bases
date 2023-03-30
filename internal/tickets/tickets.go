980bb927feeaa51ce0fb61174f725b14c6801d64
NPG27 <nicolas.potes@mercadolibre.com.co>
2023-03-30 03:59:41 -0500

    Ejercicios completados

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -1,13 +1,128 @@
 package tickets
 
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Ticket struct
 type Ticket struct {
+	id             uint16
+	passengerName  string
+	passengerEmail string
+	destination    string
+	time           string
+	price          uint16
 }
 
-// ejemplo 1
-func GetTotalTickets(destination string) (int error) {}
+func LoadTickets(filename string) ([]Ticket, error) {
+	// Leer el archivo csv
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error al abrir el archivo CSV:", err)
+		return nil, err
+	}
+	defer file.Close()
 
-// ejemplo 2
-func GetMornings(time string) (int error) {}
+	//Crear el lector de archivos CSV
+	reader := csv.NewReader(file)
+	reader.Comma = ','
 
-// ejemplo 3
-func AverageDestination(destination string, total int) (int error) {}
+	// Crear el slice de tickets
+	tickets := make([]Ticket, 0)
+
+	// Leer el archivo CSV y crear los tickets
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			if err == io.EOF {
+				// Si está al final del archivo, salir del bucle
+				break
+			}
+			fmt.Println("Error al leer una fila:", err)
+			return nil, err
+		}
+
+		// Se convierte el id de string a uint16
+		num, _ := strconv.ParseUint(record[0], 10, 16)
+		uint16num := uint16(num)
+
+		// Se convierte el precio de string a uint16
+		priceNum, _ := strconv.ParseUint(record[5], 10, 16)
+		uint16PriceNum := uint16(priceNum)
+
+		// Crear un nuevo ticket y asignar los valores
+		ticket := Ticket{
+			id:             uint16num,
+			passengerName:  record[1],
+			passengerEmail: record[2],
+			destination:    record[3],
+			time:           record[4],
+			price:          uint16PriceNum,
+		}
+
+		// Agregar el ticket al slice
+		tickets = append(tickets, ticket)
+	}
+	return tickets, nil
+}
+
+// Requerimiento 1
+func GetTotalTickets(tickets []Ticket, destination string) (int, error) {
+	total := 0
+	for _, ticket := range tickets {
+		if ticket.destination == destination {
+			total++
+		}
+	}
+	return total, nil
+}
+
+// Requerimiento 2
+func GetCountByPeriod(tickets []Ticket) map[string]int {
+	passengersByTime := map[string]int{
+		"madrugada": 0,
+		"manana":    0,
+		"tarde":     0,
+		"noche":     0,
+	}
+
+	total := 0
+	for _, ticket := range tickets {
+		hourStr := strings.Split(ticket.time, ":")[0]
+		hour, err := strconv.Atoi(hourStr)
+		if err != nil {
+			fmt.Println("Error al convertir la hora a entero:", err)
+		}
+
+		switch {
+		case hour >= 0 && hour < 6:
+			passengersByTime["madrugada"]++
+		case hour >= 7 && hour < 12:
+			passengersByTime["manana"]++
+		case hour >= 13 && hour < 19:
+			passengersByTime["tarde"]++
+		case hour >= 20 && hour <= 24:
+			passengersByTime["noche"]++
+		}
+		total++
+	}
+	return passengersByTime
+}
+
+// Requerimiento 3
+func AverageDestination(tickets []Ticket, destination string) (float64, error) {
+	totalPassengers := len(tickets)
+	totalPassengersByDestination, _ := GetTotalTickets(tickets, destination)
+
+	if totalPassengers == 0 {
+		return 0.0, fmt.Errorf("No se encontraron tickets para hoy")
+	}
+
+	average := float64(totalPassengersByDestination) / float64(totalPassengers)
+	return average, nil
+}
--- internal/tickets/tickets_test.go
+++ internal/tickets/tickets_test.go
@@ -1,5 +1,49 @@
 package tickets
 
-import "testing"
+import (
+	"testing"
 
-func TestGetTotalTickets(t *testing.T) {}
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetTotalTickets(t *testing.T) {
+	tickets, err := LoadTickets("../../tickets.csv")
+	assert.NoError(t, err, "Error al cargar los tickets para hacer tests")
+	t.Run("Prueba para destino con tiquetes", func(t *testing.T) {
+		total, err := GetTotalTickets(tickets, "Finland")
+		assert.NoError(t, err, "Error al calcular el total de tickets para Finland")
+		assert.Equal(t, 8, total, "El total de tickets para Finland debe ser 2")
+	})
+	t.Run("Prueba para destino sin tiquetes", func(t *testing.T) {
+		total, err := GetTotalTickets(tickets, "Qatar")
+		assert.NoError(t, err, "Error al calcular el total de tickets para Qatar")
+		assert.Equal(t, 0, total, "El total de tickets para Qatar debe ser 0")
+	})
+}
+
+func TestGetCountByPeriod(t *testing.T) {
+	tickets, err := LoadTickets("../../tickets.csv")
+	assert.NoError(t, err, "Error al cargar los tickets para hacer tests")
+	passengersByTime := GetCountByPeriod(tickets)
+	t.Run("Prueba para cantidad de pasajeros en la madrugada", func(t *testing.T) {
+		assert.Equal(t, 260, passengersByTime["madrugada"], "El total de pasajeros en madrugada debe ser 260")
+	})
+	t.Run("Prueba para cantidad de pasajeros en la noche", func(t *testing.T) {
+		assert.Equal(t, 151, passengersByTime["noche"], "El total de pasajeros en noche debe ser 151")
+	})
+}
+
+func TestAverageDestination(t *testing.T) {
+	tickets, err := LoadTickets("../../tickets.csv")
+	assert.NoError(t, err, "Error al cargar los tickets para hacer tests")
+	t.Run("Prueba para promedio de pasajeros para Finland", func(t *testing.T) {
+		average, err := AverageDestination(tickets, "Finland")
+		assert.NoError(t, err, "Error al calcular el promedio de pasajeros para Finland")
+		assert.Equal(t, 0.008, average, "El promedio de pasajeros para Finland debe ser 0.008")
+	})
+	t.Run("Prueba para promedio de pasajeros para Brazil", func(t *testing.T) {
+		average, err := AverageDestination(tickets, "Brazil")
+		assert.NoError(t, err, "Error al calcular el promedio de pasajeros para Brazil")
+		assert.Equal(t, 0.045, average, "El promedio de pasajeros para Brazil debe ser 0.045")
+	})
+}
